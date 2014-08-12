a9f36a9f5e5c059e6b4dd081c6f99fd6b6152a53
Igor Dejanovic <igor.dejanovic@gmail.com>
2014-08-12 21:16:33 +0200

    Hello example update

--- examples/hello_world/hello.py
+++ examples/hello_world/hello.py
@@ -1,13 +1,15 @@
 from textx import parser_from_file
-from arpeggio.export import PTDOTExporter
+from textx.export import metamodel_export
 
 # Construct parser
-parser = parser_from_file('hello.tx', debug=True)
+parser = parser_from_file('hello.tx')
 
 # Get model
 hello_model = parser.get_model('example.hello')
+# Get metamodel
+hello_metamodel = parser.get_metamodel()
+# Export metamodel to dot
+metamodel_export(hello_metamodel, 'example_meta.dot')
 
-PTDOTExporter().exportFile(parser.parse_tree, 'example_parse_tree.dot')
 
-print(hello_model)
 
