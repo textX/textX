c14029f70e8423d157b83f908ced5d22169033b4
Igor Dejanovic <igor.dejanovic@gmail.com>
2014-08-17 01:56:08 +0200

    Version bump to 0.2-dev

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup
 
 __author__ = "Igor R. Dejanović <igor DOT dejanovic AT gmail DOT com>"
-__version__ = "0.1"
+__version__ = "0.2-dev"
 
 NAME = 'textX'
 DESC = 'Meta-language in Arpeggio inspired by Xtext'
