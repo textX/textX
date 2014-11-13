2404f0226d405dac8c5c68e6d5aad8d81f81ada9
Igor Dejanovic <igor.dejanovic@gmail.com>
2014-11-13 18:54:03 +0100

    Updated robot example to align it with tutorial.

--- examples/robot/robot.py
+++ examples/robot/robot.py
@@ -1,9 +1,65 @@
 from textx.metamodel import metamodel_from_file
 from textx.export import metamodel_export, model_export
 
-robot_mm = metamodel_from_file('robot.tx', debug=False)
-metamodel_export(robot_mm, 'robot_meta.dot')
 
-robot_model = robot_mm.model_from_file('program.rbt')
-model_export(robot_model, 'program.dot')
+def move_command_processor(move_cmd):
+    """
+    This is object processor for MoveCommand instances.
+    It implements a default step of 1 in case not given
+    in the program.
+    """
+
+    if move_cmd.steps == 0:
+        move_cmd.steps = 1
+
+
+class Robot(object):
+
+    def __init__(self):
+        # Initial position is (0,0)
+        self.x = 0
+        self.y = 0
+
+    def __str__(self):
+        return "Robot position is {}, {}.".format(self.x, self.y)
+
+    def interpret(self, model):
+
+        # model is an instance of Program
+        for c in model.commands:
+
+            if c.__class__.__name__ == "InitialCommand":
+                print("Setting position to: {}, {}".format(c.x, c.y))
+                self.x = c.x
+                self.y = c.y
+            else:
+                dir = c.direction
+                print("Going {} for {} step(s).".format(dir, c.steps))
+
+                move = {
+                    "up": (0, 1),
+                    "down": (0, -1),
+                    "left": (-1, 0),
+                    "right": (1, 0)
+                }[dir]
+
+                # Calculate new robot position
+                self.x += c.steps * move[0]
+                self.y += c.steps * move[1]
+
+            print(self)
+
+if __name__ == "__main__":
+
+    robot_mm = metamodel_from_file('robot.tx', debug=False)
+    metamodel_export(robot_mm, 'robot_meta.dot')
+
+    # Register object processor for MoveCommand
+    robot_mm.register_obj_processors({'MoveCommand': move_command_processor})
+
+    robot_model = robot_mm.model_from_file('program.rbt')
+    model_export(robot_model, 'program.dot')
+
+    robot = Robot()
+    robot.interpret(robot_model)
 
