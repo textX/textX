c8253c550dd50003a2b34e46bc4407c036a59aaf
Igor Dejanovic <igor.dejanovic@gmail.com>
2014-12-29 15:35:21 +0100

    Unit tests for metamodel parsing parameters.

--- tests/functional/test_metamodel_params.py
+++ tests/functional/test_metamodel_params.py
@@ -4,15 +4,51 @@ from textx.exceptions import TextXSyntaxError
 
 
 def test_ignore_case():
-    pass
+    langdef = """
+    Model: 'start' rules*='first' 'second';
+    """
+    meta = metamodel_from_str(langdef)
+
+    # By default case is not ignored.
+    with pytest.raises(TextXSyntaxError):
+        meta.model_from_str('Start first First Second')
+
+    meta = metamodel_from_str(langdef, ignore_case=True)
+    meta.model_from_str('Start first First Second')
 
 
 def test_skipws():
-    pass
+    langdef = """
+    Model: 'start' rules*='first' 'second';
+    """
+    meta = metamodel_from_str(langdef)
+
+    # By default ws are skipped.
+    meta.model_from_str('start first first second')
+
+    meta = metamodel_from_str(langdef, skipws=False)
+    with pytest.raises(TextXSyntaxError):
+        meta.model_from_str('start first first second')
+
+    meta.model_from_str('startfirstfirstsecond')
 
 
 def test_ws():
-    pass
+    langdef = """
+    Model: 'start' rules*='first' 'second';
+    """
+    meta = metamodel_from_str(langdef)
+
+    # Default ws are space, tab and newline
+    meta.model_from_str("""start  first 
+            first second""")
+
+    meta = metamodel_from_str(langdef, ws=' ')
+    with pytest.raises(TextXSyntaxError):
+        meta.model_from_str("""start  first 
+                first second""")
+
+    meta.model_from_str('start first first second')
 
 
 def test_autokwd():
