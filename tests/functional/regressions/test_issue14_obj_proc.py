8eac7e92b7f5d30dfe0ed2e0e511852a26cb89e7
Igor Dejanovic <igor.dejanovic@gmail.com>
2015-09-22 21:32:50 +0200

    Refs #14 A test for issue 14.

---
+++ tests/functional/regressions/test_issue14_obj_proc.py
@@ -0,0 +1,53 @@
+from __future__ import unicode_literals
+import pytest
+
+from textx.metamodel import metamodel_from_str
+from textx.export import metamodel_export, model_export
+
+
+def test_issue_14():
+    """
+    Test object processors in context of match rules with base types.
+    """
+
+    grammar = """
+        Program:
+        'begin'
+            commands*=Command
+        'end'
+        ;
+
+        Command:
+        InitialCommand | InteractCommand
+        ;
+
+        InitialCommand:
+        'initial' x=INT ',' y=INT
+        ;
+
+        InteractCommand:
+            'sleep' | INT | FLOAT | BOOL | STRING
+        ;
+    """
+
+    mm = metamodel_from_str(grammar)
+    metamodel_export(mm, 'test_issue_14_metamodel.dot')
+
+    # Error happens only when there are obj. processors registered
+    mm.register_obj_processors({'InitialCommand': lambda x: x})
+
+    model_str = """
+        begin
+            initial 2, 3
+            sleep
+            34
+            4.3
+            true
+            "a string"
+        end
+    """
+    model = mm.model_from_str(model_str)
+    model_export(model, 'test_issue_14_model.dot')
+
+
+
