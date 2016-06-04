c4b4cdb22a67f17015fcfb1f696ee74024700fcc
Igor Dejanovic <igor.dejanovic@gmail.com>
2016-06-04 16:36:39 +0200

    Expression language examples. Calc and Bool.

---
+++ examples/expression/boolean.py
@@ -0,0 +1,116 @@
+from os.path import join, dirname
+from textx.metamodel import metamodel_from_str
+from textx.export import metamodel_export, model_export
+
+grammar = '''
+Bool: assignments*=Assignment expression=Or;
+Assignment: variable=ID '=' expression=Or';';
+Or: op=And ('or' op=And)*;
+And: op=Not ('and' op=Not)*;
+Not: _not?='not' op=Operand;
+Operand: op=BOOL | op=ID | ( '(' op=Or ')' );
+'''
+
+# Global variable namespace
+namespace = {}
+
+
+class Bool(object):
+    def __init__(self, **kwargs):
+        self.assignments = kwargs.pop('assignments')
+        self.expression = kwargs.pop('expression')
+
+    @property
+    def value(self):
+        # Evaluate variables in the order of definition
+        for a in self.assignments:
+            namespace[a.variable] = a.expression.value
+        return self.expression.value
+
+
+class ExpressionElement(object):
+    def __init__(self, **kwargs):
+
+        # textX will pass in parent attribute used for parent-child
+        # relationships. We can use it if we want to.
+        self.parent = kwargs.get('parent', None)
+
+        # We have 'op' attribute in all grammar rules
+        self.op = kwargs['op']
+
+        super(ExpressionElement, self).__init__()
+
+
+class Or(ExpressionElement):
+    @property
+    def value(self):
+        ret = self.op[0].value
+        for operand in self.op[1:]:
+                ret = ret or operand.value
+        return ret
+
+
+class And(ExpressionElement):
+    @property
+    def value(self):
+        ret = self.op[0].value
+        for operand in self.op[1:]:
+            ret = ret and operand.value
+        return ret
+
+
+class Not(ExpressionElement):
+    def __init__(self, **kwargs):
+        self._not = kwargs.pop('_not')
+        super(Not, self).__init__(**kwargs)
+
+    @property
+    def value(self):
+        ret = self.op.value
+        return not ret if self._not else ret
+
+
+class Operand(ExpressionElement):
+    @property
+    def value(self):
+        op = self.op[0]
+        if type(op) is bool:
+            return op
+        elif op in namespace:
+            return namespace[op]
+        else:
+            raise Exception('Unknown variable "{}" at position {}'
+                            .format(op, self._tx_position))
+
+
+def main(debug=False):
+
+    calc_mm = metamodel_from_str(grammar,
+                                 classes=[Bool, Or, And, Not, Operand],
+                                 ignore_case=True,
+                                 debug=debug)
+
+    this_folder = dirname(__file__)
+    if debug:
+        metamodel_export(calc_mm, join(this_folder, 'bool_metamodel.dot'))
+
+    input_expr = '''
+        a = true;
+        b = not a and true;
+        a and false or not b
+    '''
+
+    model = calc_mm.model_from_str(input_expr)
+
+    if debug:
+        model_export(model, join(this_folder, 'bool_model.dot'))
+
+    # Getting value property from the Bool instance will start evaluation.
+    result = model.value
+
+    assert model.value is True
+    print("Result is", result)
+
+
+if __name__ == '__main__':
+    main()
---
+++ examples/expression/calc.py
@@ -0,0 +1,126 @@
+from os.path import join, dirname
+from textx.metamodel import metamodel_from_str
+from textx.export import metamodel_export, model_export
+
+grammar = '''
+Calc: assignments*=Assignment expression=Expression;
+Assignment: variable=ID '=' expression=Expression ';';
+Expression: op=Term (op=PlusOrMinus op=Term)* ;
+PlusOrMinus: '+' | '-';
+Term: op=Factor (op=MulOrDiv op=Factor)*;
+MulOrDiv: '*' | '/' ;
+Factor: (sign=PlusOrMinus)?  op=Operand;
+Operand: op=NUMBER | op=ID | ('(' op=Expression ')');
+'''
+
+# Global variable namespace
+namespace = {}
+
+
+class Calc(object):
+    def __init__(self, **kwargs):
+        self.assignments = kwargs.pop('assignments')
+        self.expression = kwargs.pop('expression')
+
+    @property
+    def value(self):
+        # Evaluate variables in the order of definition
+        for a in self.assignments:
+            namespace[a.variable] = a.expression.value
+        return self.expression.value
+
+
+class ExpressionElement(object):
+    def __init__(self, **kwargs):
+
+        # textX will pass in parent attribute used for parent-child
+        # relationships. We can use it if we want to.
+        self.parent = kwargs.get('parent', None)
+
+        # We have 'op' attribute in all grammar rules
+        self.op = kwargs['op']
+
+        super(ExpressionElement, self).__init__()
+
+
+class Factor(ExpressionElement):
+    def __init__(self, **kwargs):
+        self.sign = kwargs.pop('sign', '+')
+        super(Factor, self).__init__(**kwargs)
+
+    @property
+    def value(self):
+        value = self.op.value
+        return -value if self.sign == '-' else value
+
+
+class Term(ExpressionElement):
+    @property
+    def value(self):
+        ret = self.op[0].value
+        for operation, operand in zip(self.op[1::2], self.op[2::2]):
+            if operation == '*':
+                ret *= operand.value
+            else:
+                ret /= operand.value
+        return ret
+
+
+class Expression(ExpressionElement):
+    @property
+    def value(self):
+        ret = self.op[0].value
+        for operation, operand in zip(self.op[1::2], self.op[2::2]):
+            if operation == '+':
+                ret += operand.value
+            else:
+                ret -= operand.value
+        return ret
+
+
+class Operand(ExpressionElement):
+    @property
+    def value(self):
+        op = self.op[0]
+        if type(op) in {int, float}:
+            return op
+        elif isinstance(op, ExpressionElement):
+            return op.value
+        elif op in namespace:
+            return namespace[op]
+        else:
+            raise Exception('Unknown variable "{}" at position {}'
+                            .format(op, self._tx_position))
+
+
+def main(debug=False):
+
+    calc_mm = metamodel_from_str(grammar,
+                                 classes=[Calc, Expression, Term, Factor,
+                                          Operand],
+                                 debug=debug)
+
+    this_folder = dirname(__file__)
+    if debug:
+        metamodel_export(calc_mm, join(this_folder, 'calc_metamodel.dot'))
+
+    input_expr = '''
+        a = 10;
+        b = 2 * a + 17;
+        -(4-1)*a+(2+4.67)+b*5.89/(.2+7)
+    '''
+
+    model = calc_mm.model_from_str(input_expr)
+
+    if debug:
+        model_export(model, join(this_folder, 'calc_model.dot'))
+
+    # Getting value property from the Calc instance will start evaluation.
+    result = model.value
+
+    assert (model.value - 6.93805555) < 0.0001
+    print("Result is", result)
+
+
+if __name__ == '__main__':
+    main()
