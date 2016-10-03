66c4825e0d5120ac00010247d7355a67dc56f086
Igor Dejanovic <igor.dejanovic@gmail.com>
2016-10-03 21:38:38 +0200

    Improved setup.py. Use wheel. Use twine for upload.

--- setup.py
+++ setup.py
@@ -1,22 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
+import sys
 import codecs
 from setuptools import setup
-
-__author__ = "Igor R. Dejanović <igor DOT dejanovic AT gmail DOT com>"
-__version__ = "1.4"
+import textx
 
 NAME = 'textX'
 DESC = 'Meta-language for DSL implementation inspired by Xtext'
-VERSION = __version__
+VERSION = textx.__version__
 AUTHOR = 'Igor R. Dejanovic'
 AUTHOR_EMAIL = 'igor DOT dejanovic AT gmail DOT com'
 LICENSE = 'MIT'
 URL = 'https://github.com/igordejanovic/textX'
-DOWNLOAD_URL = 'https://github.com/igordejanovic/textX/archive/v%s.tar.gz' % VERSION
+DOWNLOAD_URL = 'https://github.com/igordejanovic/textX/archive/v%s.tar.gz' \
+    % VERSION
 README = codecs.open(os.path.join(os.path.dirname(__file__), 'README.rst'),
                      'r', encoding='utf-8').read()
+
+
+if sys.argv[-1].startswith('publish'):
+    if os.system("pip list | grep wheel"):
+        print("wheel not installed.\nUse `pip install wheel`.\nExiting.")
+        sys.exit()
+    if os.system("pip list | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    os.system("python setup.py sdist bdist_wheel")
+    if sys.argv[-1] == 'publishtest':
+        os.system("twine upload -r test dist/*")
+    else:
+        os.system("twine upload dist/*")
+        print("You probably want to also tag the version now:")
+        print("  git tag -a {0} -m 'version {0}'".format(VERSION))
+        print("  git push --tags")
+    sys.exit()
 
 setup(
     name = NAME,
