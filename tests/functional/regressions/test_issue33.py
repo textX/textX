a4f12af53c8dec9e037081fa9ee48921ef6950ef
Igor Dejanovic <igor.dejanovic@gmail.com>
2017-04-27 14:43:43 +0200

    Regression tests for issue 33.

---
+++ tests/functional/regressions/test_issue33.py
@@ -0,0 +1,76 @@
+from __future__ import unicode_literals
+import pytest  # noqa
+from textx.metamodel import metamodel_from_str
+
+
+def test_issue_33_parentheses():
+
+    grammar = """
+    Method:
+        'func('  (params+=Parameter[','])?  ')'
+    ;
+    Parameter:
+        (type=ID name=ID)  |  name=ID
+    ;
+    """
+
+    meta_model = metamodel_from_str(grammar)
+    model = meta_model.model_from_str(
+        """
+        func( a b, c )
+        """
+    )
+    assert model
+    assert model.params[0].type == 'a'
+    assert model.params[0].name == 'b'
+
+    assert model.params[1].type == ''
+    assert model.params[1].name == 'c'
+
+
+def test_issue_33_alternatives():
+    grammar = """
+    Method:
+        'func('  (params+=Parameter[','])?  ')'
+    ;
+    Parameter:
+        type=ID name=ID  |  name=ID
+    ;
+    """
+
+    meta_model = metamodel_from_str(grammar)
+    model = meta_model.model_from_str(
+        """
+        func( a b, c )
+        """
+    )
+    assert model
+    assert model.params[0].type == 'a'
+    assert model.params[0].name == 'b'
+
+    assert model.params[1].type == ''
+    assert model.params[1].name == 'c'
+
+
+def test_issue_33_alternatives_with_arbitrary_named_attr():
+    grammar = """
+    Method:
+        'func('  (params+=Parameter[','])?  ')'
+    ;
+    Parameter:
+        type=ID my_name=ID  |  my_name=ID
+    ;
+    """
+
+    meta_model = metamodel_from_str(grammar)
+    model = meta_model.model_from_str(
+        """
+        func( a b, c )
+        """
+    )
+    assert model
+    assert model.params[0].type == 'a'
+    assert model.params[0].my_name == 'b'
+
+    assert model.params[1].type == ''
+    assert model.params[1].my_name == 'c'
