60d362a97437db11ccd91f2b7664840ad7728dd3
Igor Dejanovic <igor.dejanovic@gmail.com>
2017-10-28 15:23:32 +0200

    Some more funcs and classes imported to root package for convenience.

--- textx/__init__.py
+++ textx/__init__.py
@@ -1,4 +1,7 @@
-from textx.metamodel import metamodel_from_file, metamodel_from_str  # noqa
-from textx.langapi import get_language, iter_languages  # noqa
+from textx.metamodel import metamodel_from_file, metamodel_from_str
+from textx.model import children_of_type, parent_of_type, model_root, metamodel
+from textx.exceptions import TextXError, TextXSyntaxError, \
+    TextXSemanticError
+from textx.langapi import get_language, iter_languages
 
 __version__ = "1.6.dev"
