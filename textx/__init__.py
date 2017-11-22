84bfd8748554237dd2a0919c45e761523a4e2712
Igor Dejanovic <igor.dejanovic@gmail.com>
2017-11-22 19:35:38 +0100

    Fixing build.

--- textx/__init__.py
+++ textx/__init__.py
@@ -3,5 +3,3 @@ from textx.model import children_of_type, parent_of_type, model_root, metamodel
 from textx.exceptions import TextXError, TextXSyntaxError, \
     TextXSemanticError
 from textx.langapi import get_language, iter_languages
-
-__version__ = "1.6.dev"
