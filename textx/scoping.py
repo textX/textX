b48ff7b5faefc16b4de27a9a9db68b7891105b3f
goto40 <goto40@users.noreply.github.com>
2017-12-18 21:16:25 +0100

    moved functions

--- textx/scoping.py
+++ textx/scoping.py
@@ -55,52 +55,9 @@ We provide some standard scope providers:
   * ScopeProviderPlainNamesWithGlobalRepo (decorated scope provider)
 """
 
-
-def _find_obj_fqn(p, fqn_name):
-    """
-    Helper function:
-    find a named object based on a qualified name ("."-separated names) starting from object p.
-    :param p: the container where to start the search
-    :param fqn_name: the "."-separated name
-    :return: the object or None
-    """
-    def find_obj(parent, name):
-        for attr in [a for a in dir(parent) if not a.startswith('__')
-                                               and not a.startswith('_tx_')
-                                               and not callable(getattr(parent, a))]:
-            obj = getattr(parent, attr)
-            if isinstance(obj, (list, tuple)):
-                for innerobj in obj:
-                    if "name" in dir(innerobj) and innerobj.name == name: return innerobj;
-            else:
-                if "name" in dir(obj) and obj.name == name: return obj;
-        return None
-
-    for n in str.split(fqn_name,'.'):
-        obj = find_obj(p,n)
-        if obj:
-            p = obj;
-        else:
-            return None;
-    return p;
-
-
-def _find_referenced_obj(p, name):
-    """
-    Helper function:
-    Search the fully qualified name starting at relative container p.
-    If no result is found, the search is continued from p.parent until the model root is reached.
-    :param m: parent object
-    :param n: name to be found
-    :return: None or the found object
-    """
-    ret = _find_obj_fqn(p, name)
-    if ret: return ret;
-    while hasattr(p, "parent"):
-        p = p.parent
-        ret = _find_obj_fqn(p, name)
-        if ret: return ret;
-    return None
+# -------------------------------------------------------------------------------------
+# Scope helper classes:
+# -------------------------------------------------------------------------------------
 
 
 class ModelRepository:
@@ -202,6 +159,11 @@ class GlobalModelRepository:
         self.all_models.filename_to_model[filename] = other_model
 
 
+# -------------------------------------------------------------------------------------
+# Scope providers:
+# -------------------------------------------------------------------------------------
+
+
 def scope_provider_plain_names(parser, obj, attr, obj_ref):
     """
     the default scope provider
@@ -275,6 +237,50 @@ def scope_provider_fully_qualified_names(parser,obj,attr,obj_ref):
     :param obj_ref: ObjCrossRef to be resolved
     :returns None or the referenced object
     """
+    def _find_obj_fqn(p, fqn_name):
+        """
+        Helper function:
+        find a named object based on a qualified name ("."-separated names) starting from object p.
+        :param p: the container where to start the search
+        :param fqn_name: the "."-separated name
+        :return: the object or None
+        """
+        def find_obj(parent, name):
+            for attr in [a for a in dir(parent) if not a.startswith('__')
+                                                   and not a.startswith('_tx_')
+                                                   and not callable(getattr(parent, a))]:
+                obj = getattr(parent, attr)
+                if isinstance(obj, (list, tuple)):
+                    for innerobj in obj:
+                        if "name" in dir(innerobj) and innerobj.name == name: return innerobj;
+                else:
+                    if "name" in dir(obj) and obj.name == name: return obj;
+            return None
+
+        for n in str.split(fqn_name, '.'):
+            obj = find_obj(p, n)
+            if obj:
+                p = obj;
+            else:
+                return None;
+        return p;
+    def _find_referenced_obj(p, name):
+        """
+        Helper function:
+        Search the fully qualified name starting at relative container p.
+        If no result is found, the search is continued from p.parent until the model root is reached.
+        :param m: parent object
+        :param n: name to be found
+        :return: None or the found object
+        """
+        ret = _find_obj_fqn(p, name)
+        if ret: return ret;
+        while hasattr(p, "parent"):
+            p = p.parent
+            ret = _find_obj_fqn(p, name)
+            if ret: return ret;
+        return None
+
     from textx.model import ObjCrossRef
     assert type(obj_ref) is ObjCrossRef, type(obj_ref)
     cls, obj_name = obj_ref.cls, obj_ref.obj_name
