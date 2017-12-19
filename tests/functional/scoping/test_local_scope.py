be33c05ac6cfba5b5d713ef5445896a41e523f95
goto40 <goto40@users.noreply.github.com>
2017-12-19 21:09:01 +0100

    added a test detail with a list of references (fully qualified named scoping)

--- tests/functional/scoping/test_local_scope.py
+++ tests/functional/scoping/test_local_scope.py
@@ -1,9 +1,10 @@
 from textx.metamodel import metamodel_from_file
+from textx.model import children_of_type
 import textx.scoping as scoping
 from os.path import dirname, abspath
 import textx.exceptions
 from pytest import raises
-
+from tests.functional.scoping.test_import_module import get_unique_named_object
 
 def test_postponed_resolution_error():
     #################################
@@ -38,8 +39,8 @@ def test_model_with_local_scope():
     my_meta_model = metamodel_from_file(abspath(dirname(__file__)) + '/components_model1/Components.tx')
     my_meta_model.register_scope_provider({
         "*.*":scoping.scope_provider_fully_qualified_names,
-        "Connection.from_port":scoping.ScopeProviderForSimpleRelativeNamedLookups("from.component","slots"),
-        "Connection.to_port":scoping.ScopeProviderForSimpleRelativeNamedLookups("to.component","slots")
+        "Connection.from_port":scoping.ScopeProviderForSimpleRelativeNamedLookups("from_inst.component","slots"),
+        "Connection.to_port":scoping.ScopeProviderForSimpleRelativeNamedLookups("to_inst.component","slots")
     })
 
 
@@ -53,7 +54,21 @@ def test_model_with_local_scope():
     # TEST MODEL
     #################################
 
-    # TODO
+    # test local refs
+    action2 = get_unique_named_object(my_model, "action2")
+    action3 = get_unique_named_object(my_model, "action3")
+    connections = children_of_type("Connection",my_model)
+    selected_connections = list(filter(lambda x:x.from_inst==action2 and x.to_inst==action3, connections))
+    assert len(selected_connections)==1
+
+    # test list of formats
+    input2 = get_unique_named_object(my_model, "input2")
+    assert len(input2.formats)==3
+    format_names = map( lambda x:x.name, input2.formats )
+    assert "A" in format_names
+    assert "B" in format_names
+    assert "C" in format_names
+    assert not "D" in format_names
 
     #################################
     # END
@@ -64,20 +79,20 @@ def test_model_with_local_scope_postponed():
     # META MODEL DEF
     #################################
 
-    sp1 = scoping.ScopeProviderForSimpleRelativeNamedLookups("from.component","slots")
+    sp1 = scoping.ScopeProviderForSimpleRelativeNamedLookups("from_inst.component","slots")
     my_meta_model1 = metamodel_from_file(abspath(dirname(__file__)) + '/components_model1/Components.tx')
     my_meta_model1.register_scope_provider({
         "*.*":scoping.scope_provider_fully_qualified_names,
         "Connection.from_port":sp1,
-        "Connection.to_port":scoping.ScopeProviderForSimpleRelativeNamedLookups("to.component","slots")
+        "Connection.to_port":scoping.ScopeProviderForSimpleRelativeNamedLookups("to_inst.component","slots")
     })
 
-    sp2 = scoping.ScopeProviderForSimpleRelativeNamedLookups("from.component","slots")
+    sp2 = scoping.ScopeProviderForSimpleRelativeNamedLookups("from_inst.component","slots")
     my_meta_model2 = metamodel_from_file(abspath(dirname(__file__)) + '/components_model2/Components.tx')
     my_meta_model2.register_scope_provider({
         "*.*":scoping.scope_provider_fully_qualified_names,
         "Connection.from_port":sp2,
-        "Connection.to_port":scoping.ScopeProviderForSimpleRelativeNamedLookups("to.component","slots")
+        "Connection.to_port":scoping.ScopeProviderForSimpleRelativeNamedLookups("to_inst.component","slots")
     })
 
 
