df0c7a74cb1038d99285efa77ad9ed77ffd2f435
goto40 <goto40@users.noreply.github.com>
2018-01-17 20:12:18 +0100

    simplified code.

--- examples/CustomIDL/support/attributes.py
+++ examples/CustomIDL/support/attributes.py
@@ -22,6 +22,7 @@ class BaseStruct(object):
                 if isinstance(self.__dict__[key], AttributeBase):
                     self.__dict__[key]._activate_read_only()
 
+
     def _set_value(self, item, new_value):
         print("internal 0SET VALUE {}".format(item))
         if item in self.__dict__.keys():
@@ -33,11 +34,7 @@ class BaseStruct(object):
         if self._read_only:
             raise Exception("illegal acces to read only attribute")
         else:
-            print("0SET VALUE {}".format(item))
-            if item in self.__dict__.keys():
-                self.__dict__[item].__set__( self, new_value )
-            else:
-                raise Exception("unexpected: no attribute {} in {}".format(item, type(self)))
+            self._set_value(item, new_value)
 
 
 class AttributeBase(object):
@@ -65,29 +62,6 @@ class ReadOnlyAttribute(ScalarAttributeBase):
         if isinstance(self._value, BaseStruct):
             self._value._activate_read_only()
 
-    def __set__(self, instance, new_value):
-        if self._read_only:
-            raise Exception("illegal acces to read only attribute")
-        else:
-            self._set_value(new_value)
-
-#    def __get__(self, instance, owner):
-#        print("FINAL GETTER getting value {}".format(self.__dict__["_value"]))
-#        return self.__dict__["_value"]
-
-    def __delattr__(self, item, _):
-        raise Exception("illegal acces to read only attribute")
-
-    def __getattribute__(self, item):
-        ret = super(ReadOnlyAttribute, self).__getattribute__(item)
-        if not item.startswith("_"):
-            print("GET {}".format(item))
-            if isinstance(ret, ScalarAttributeBase):
-                ret = ret._value
-            elif isinstance(ret, ArrayAttributeBase):
-                ret = ret._get_view()
-        return ret
-
     def _set_value(self, new_value):
         if type(new_value) is type(self._value):
             print("SETTER setting value {}".format(new_value))
@@ -99,15 +73,11 @@ class ReadOnlyAttribute(ScalarAttributeBase):
         else:
             raise Exception("unexpected type {} for {}".format(type(new_value), type(self._value)))
 
-
-    def __setattr__(self, item, new_value):
+    def __set__(self, instance , new_value):
         if self._read_only:
             raise Exception("illegal acces to read only attribute")
         else:
-            if item in self._value.__dict__.keys():
-                self._value.__dict__[item] = new_value
-            else:
-                raise Exception("unexpected: no attribute {} in {}".format(item, type(self._value)))
+            self._set_value(new_value)
 
 
 class Attribute(ReadOnlyAttribute):
@@ -134,9 +104,3 @@ class DynamicArrayAttribute(ArrayAttributeBase):
 
     def _get_view(self):
         return self._value.view()
-
-#    def __get__(self, instance, owner):
-#        return self._get_view()
-
-#    def __getattr__(self, item):
-#        return self._value[item]
