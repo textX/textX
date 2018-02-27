a50df3b9e2e59634b81f0a52a77b89848b86f69a
goto40 <goto40@users.noreply.github.com>
2018-02-27 19:11:22 +0100

    pull-request discussion: reverted erroneous changes

--- examples/Entity/entity_codegen.py
+++ examples/Entity/entity_codegen.py
@@ -6,62 +6,49 @@ from os import mkdir
 from os.path import exists, dirname, join
 import jinja2
 from entity_test import get_entity_mm
-import sys
 
 
-def main(filename=None, debug=False):
-    try:
-        this_folder = dirname(__file__)
-        if not filename: filename = join(this_folder, 'example.myidl')
+def main(debug=False):
 
-        entity_mm = get_entity_mm(debug)
+    this_folder = dirname(__file__)
 
-        # Build Person model from person.ent file
-        person_model = entity_mm.model_from_file(filename)
+    entity_mm = get_entity_mm(debug)
 
-        def javatype(s):
-            """
-            Maps type names from PrimitiveType to Java.
-            """
-            return {
-                    'integer': 'int',
-                    'string': 'String'
-            }.get(s.name, s.name)
+    # Build Person model from person.ent file
+    person_model = entity_mm.model_from_file(join(this_folder, 'person.ent'))
 
-        # Create output folder
-        srcgen_folder = join(this_folder, 'srcgen')
-        if not exists(srcgen_folder):
-            mkdir(srcgen_folder)
+    def javatype(s):
+        """
+        Maps type names from PrimitiveType to Java.
+        """
+        return {
+                'integer': 'int',
+                'string': 'String'
+        }.get(s.name, s.name)
 
-        # Initialize template engine.
-        jinja_env = jinja2.Environment(
-            loader=jinja2.FileSystemLoader(this_folder),
-            trim_blocks=True,
-            lstrip_blocks=True)
+    # Create output folder
+    srcgen_folder = join(this_folder, 'srcgen')
+    if not exists(srcgen_folder):
+        mkdir(srcgen_folder)
 
-        # Register filter for mapping Entity type names to Java type names.
-        jinja_env.filters['javatype'] = javatype
+    # Initialize template engine.
+    jinja_env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(this_folder),
+        trim_blocks=True,
+        lstrip_blocks=True)
 
-        # Load Java template
-        template = jinja_env.get_template('java.template')
+    # Register filter for mapping Entity type names to Java type names.
+    jinja_env.filters['javatype'] = javatype
 
-        for entity in person_model.entities:
-            # For each entity generate java file
-            with open(join(srcgen_folder,
-                           "%s.java" % entity.name.capitalize()), 'w') as f:
-                f.write(template.render(entity=entity))
-        return 0 # no error
-    except:
-        e = sys.exc_info()[0]
-        print("Error: {}".format(e))
-        return 1
+    # Load Java template
+    template = jinja_env.get_template('java.template')
+
+    for entity in person_model.entities:
+        # For each entity generate java file
+        with open(join(srcgen_folder,
+                       "%s.java" % entity.name.capitalize()), 'w') as f:
+            f.write(template.render(entity=entity))
 
 
 if __name__ == "__main__":
-    if len(sys.argv)==1:
-        exit(main())
-    elif len(sys.argv)==2:
-        exit(main(sys.argv[1]))
-    else:
-        print("usage: {} <model_file>".format(sys.argv[1]))
-        exit(1)
+    main()
