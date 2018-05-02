f2ba80b06948029ad41b889f475010096705f05a
Igor Dejanovic <igor.dejanovic@gmail.com>
2018-05-02 15:57:27 +0200

    Moving and reworking match filter tests.

--- tests/functional/test_processors.py
+++ tests/functional/test_processors.py
@@ -1,8 +1,15 @@
 """
 Testing model and object processors.
 """
+from __future__ import unicode_literals
 import pytest  # noqa
+import sys
 from textx import metamodel_from_str
+
+if sys.version < '3':
+    text = unicode  # noqa
+else:
+    text = str
 
 grammar = """
 First:
@@ -111,3 +118,73 @@ def test_object_processor_replace_object():
     assert first.seconds[0] == 17
 
     assert first.c == '[dfdf]'
+
+
+def test_obj_processor_simple_match_rule():
+    grammar = """
+    First:
+        a=MyFloat 'end'
+    ;
+    MyFloat:
+        /\d+\.(\d+)?/
+    ;
+    """
+    model = '3. end'
+
+    mm = metamodel_from_str(grammar)
+    m = mm.model_from_str(model)
+    assert type(m.a) is text
+
+    processors = {
+        'MyFloat': lambda x: float(x)
+    }
+    print('filters')
+    mm = metamodel_from_str(grammar)
+    mm.register_obj_processors(processors)
+    m = mm.model_from_str(model)
+
+    assert type(m.a) is float
+
+
+def test_obj_processor_sequence_match_rule():
+
+    grammar = """
+    First:
+        i=MyFixedInt 'end'
+    ;
+    MyFixedInt:
+    '0' '0' '04'
+    ;
+    """
+
+    model = '0004 end'
+
+    mm = metamodel_from_str(grammar)
+    m = mm.model_from_str(model)
+    assert type(m.i) is text
+
+    processors = {
+        'MyFixedInt': lambda x: int(x)
+    }
+    mm = metamodel_from_str(grammar)
+    mm.register_obj_processors(processors)
+    m = mm.model_from_str(model)
+
+    assert type(m.i) is int
+
+
+def test_base_type_obj_processor_override():
+    grammar = """
+    First:
+        'begin' i=INT 'end'
+    ;
+    """
+
+    processors = {
+        'INT': lambda x: float(x)
+    }
+    mm = metamodel_from_str(grammar)
+    mm.register_obj_processors(processors)
+    m = mm.model_from_str('begin 34 end')
+
+    assert type(m.i) is float
