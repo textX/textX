2d54348ef962482d4f8b4776ec305c3a2a8ca2df
goto40 <pierre.bayerl@googlemail.com>
2018-10-16 21:03:21 +0200

    added test to demonstrate "get position of model object in model input"

---
+++ tests/functional/regressions/test_issue_89.py
@@ -0,0 +1,18 @@
+import textx
+
+def test_issue89_get_obj_pos_in_text():
+    mm = textx.metamodel_from_str('''
+        Model: objs+=Obj;
+        Obj: 'obj' name=ID;
+    ''')
+    m = mm.model_from_str('''obj A
+obj B
+ obj C
+         obj D
+    ''')
+    assert (1,1) == textx.get_model(m.objs[0])._tx_parser.pos_to_linecol(
+        m.objs[0]._tx_position)
+
+    assert (2,1) == m._tx_parser.pos_to_linecol(m.objs[1]._tx_position)
+    assert (3,2) == m._tx_parser.pos_to_linecol(m.objs[2]._tx_position)
+    assert (4,10) == m._tx_parser.pos_to_linecol(m.objs[3]._tx_position)
