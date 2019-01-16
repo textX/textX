a7296325d5cecc08eecebbef6dcd8015b5a7b337
goto40 <pierre.bayerl@googlemail.com>
2019-01-16 20:33:48 +0100

    fixed flake8

--- tests/functional/regressions/test_issue155.py
+++ tests/functional/regressions/test_issue155.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+
 def test_textx_issue155():
     import textx
 
@@ -26,16 +27,20 @@ def test_textx_issue155():
     assert textx_isinstance(m1.expression.left, METAMODEL['RawCondition'])
     assert len(m1.expression.operations) == 1
     assert m1.expression.operations[0].op.op == 'OR'
-    assert textx_isinstance(m1.expression.operations[0].remaining, METAMODEL['RawCondition'])
+    assert textx_isinstance(m1.expression.operations[0].remaining,
+                            METAMODEL['RawCondition'])
 
     m2 = METAMODEL.model_from_str('a=2 AND (b=3 OR c=4)')
     assert textx_isinstance(m2.expression, METAMODEL['BaseExpr'])
     assert textx_isinstance(m2.expression.left, METAMODEL['RawCondition'])
     assert len(m2.expression.operations) == 1
     assert m2.expression.operations[0].op.op == 'AND'
-    assert textx_isinstance(m2.expression.operations[0].remaining, METAMODEL['BaseExpr'])
-    assert textx_isinstance(m2.expression.operations[0].remaining.left, METAMODEL['RawCondition'])
-    assert len(m2.expression.operations[0].remaining.operations)==1
+    assert textx_isinstance(m2.expression.operations[0].remaining,
+                            METAMODEL['BaseExpr'])
+    assert textx_isinstance(m2.expression.operations[0].remaining.left,
+                            METAMODEL['RawCondition'])
+    assert len(m2.expression.operations[0].remaining.operations) == 1
     assert m2.expression.operations[0].remaining.operations[0].op.op == 'OR'
-    assert textx_isinstance(m2.expression.operations[0].remaining.operations[0].remaining, METAMODEL['RawCondition'])
-
+    assert textx_isinstance(m2.expression.operations[0].remaining.
+                            operations[0].remaining,
+                            METAMODEL['RawCondition'])
