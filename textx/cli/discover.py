12e6a990124b8db095ccca0f145936af1f390fd5
Igor Dejanovic <igor.dejanovic@gmail.com>
2019-03-30 12:13:19 +0100

    Simplify list generators/languages command

--- textx/cli/discover.py
+++ textx/cli/discover.py
@@ -1,22 +1,14 @@
 import click
 import pkg_resources
 
-# Registered languages keyed by their names
-languages = None
-generators = None
-
 
 def list_languages():
     """
     List all registered languages
     """
-    global languages
-    if languages is None:
-        languages = {}
-        for language in pkg_resources.iter_entry_points(
-                group='textx_languages'):
-            languages[language.name] = language.load()
-    for language in languages.values():
+    for language in pkg_resources.iter_entry_points(
+            group='textx_languages'):
+        language = language.load()
         click.echo("{}({})\t\t{}".format(language.name,
                                          language.extension,
                                          language.description))
@@ -26,13 +18,9 @@ def list_generators():
     """
     List all registered generators
     """
-    global generators
-    if generators is None:
-        generators = {}
-        for generator in pkg_resources.iter_entry_points(
-                group='textx_languages'):
-            generator[generator.name] = generator.load()
-    for generator in generators.values():
+    for generator in pkg_resources.iter_entry_points(
+            group='textx_generators'):
+        generator = generator.load()
         click.echo("{}->{}\t\t{}".format(generator.language,
                                          generator.target,
                                          generator.desciption))
