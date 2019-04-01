d34a557049ee0de696173e0f9b43085777cb5e0a
Igor Dejanovic <igor.dejanovic@gmail.com>
2019-04-01 20:20:15 +0200

    Add installation of `flow_codegen` test project

--- install-all-dev.sh
+++ install-all-dev.sh
@@ -6,3 +6,4 @@ pip install -e tests/functional/subcommands/example_project || exit 1
 pip install -e tests/functional/registration/projects/types_dsl || exit 1
 pip install -e tests/functional/registration/projects/data_dsl || exit 1
 pip install -e tests/functional/registration/projects/flow_dsl || exit 1
+pip install -e tests/functional/registration/projects/flow_codegen || exit 1
