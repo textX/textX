3e0a0f19be51969141a418f7f1261994bf40dfb6
Igor Dejanovic <igor.dejanovic@gmail.com>
2019-04-01 18:56:23 +0200

    Implement `generate` command

--- textx/__init__.py
+++ textx/__init__.py
@@ -3,17 +3,18 @@ from textx.metamodel import metamodel_from_file, metamodel_from_str
 from textx.model import get_children_of_type, get_parent_of_type, \
     get_model, get_metamodel, get_children
 from textx.exceptions import TextXError, TextXSyntaxError, \
-    TextXSemanticError
+    TextXSemanticError, TextXRegistrationError
 from textx.langapi import get_language, iter_languages
 from textx.scoping.tools import textx_isinstance
 from textx.registration import (LanguageDesc, GeneratorDesc,
                                 register_language, register_generator,
                                 clear_language_registrations,
                                 clear_generator_registrations,
+                                languages_for_file,
+                                language_for_file,
                                 metamodel_for_language,
                                 metamodel_for_file,
                                 metamodels_for_file,
-                                metamodel_for_pattern,
-                                metamodels_for_pattern)
+                                generator_for_language_target)
 
 __version__ = "1.8.0"
--- textx/cli/generate.py
+++ textx/cli/generate.py
@@ -1,5 +1,71 @@
+import sys
+import os
+import click
+from textx import (metamodel_for_file,
+                   metamodel_for_language,
+                   language_for_file,
+                   generator_for_language_target,
+                   TextXRegistrationError)
 
-def generate():
+
+@click.argument('model_files', type=click.Path(), required=True, nargs=-1)
+@click.option('--output-path', '-o', type=click.Path(), default=None,
+              help='The output to generate to. Default = same as input.')
+@click.option('--language',
+              help='A name of the language model conforms to.'
+              ' Deduced from file name if not given.')
+@click.option('--target', help='Target output format.', required=True)
+@click.option('--overwrite', is_flag=True, default=False, required=False,
+              help='Should overwrite output files if exist.')
+@click.pass_context
+def generate(ctx, model_files, output_path, language, target, overwrite):
     """
-    Run code generator on a provided model(s)
+    Run code generator on a provided model(s).
+
+    For example::
+
+    \b
+    # Generate PlantUML output from .flow models
+    textx generate mymodel.flow --target PlantUML
+
+    \b
+    # or with defined output folder
+    textx generate mymodel.flow rendered_model/ --target PlantUML
+
+    \b
+    # To chose language by name and not by file pattern use --language
+    textx generate *.flow --language flow --target PlantUML
+
+    \b
+    # Use --overwrite to overwrite target files
+    textx generate mymodel.flow --target PlantUML --overwrite
+
+    \b
+    # In all above cases PlantUML generator must be registered:
+    $ textx list-generators
+    flow-dsl -> PlantUML  Generating PlantUML visualization from flow-dsl
     """
+
+    if not model_files:
+        click.echo(ctx.get_help())
+        sys.exit(1)
+
+    click.echo('Generating {} target from models:'.format(target))
+
+    for model_file in model_files:
+        click.echo(os.path.abspath(model_file))
+        if language:
+            metamodel = metamodel_for_language(language)
+        else:
+            language = language_for_file(model_file).name
+            metamodel = metamodel_for_file(model_file)
+
+        model = metamodel.model_from_file(model_file)
+
+        try:
+            generator = generator_for_language_target(language, target)
+        except TextXRegistrationError as e:
+            click.echo(e.message)
+            sys.exit(1)
+
+        generator(model, output_path, overwrite)
