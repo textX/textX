0454d87e21c18d30ae534a0e16632c5348a7f317
Igor Dejanovic <igor.dejanovic@gmail.com>
2019-04-01 19:49:39 +0200

    PEP-8 and variable name fix for `check` tests

--- tests/functional/registration/test_check.py
+++ tests/functional/registration/test_check.py
@@ -13,25 +13,26 @@ def test_check_metamodel():
     """
 
     metamodel_file = os.path.join(this_folder,
-                                  'projects', 'flow_dsl', 'flow_dsl', 'Flow.tx')
+                                  'projects', 'flow_dsl', 'flow_dsl',
+                                  'Flow.tx')
     output = subprocess.check_output(['textx', 'check', metamodel_file],
                                      stderr=subprocess.STDOUT)
     assert b'Flow.tx: OK.' in output
 
 
 def test_check_valid_model():
-    metamodel_file = os.path.join(this_folder,
-                                  'projects', 'flow_dsl', 'tests',
-                                  'models', 'data_flow.eflow')
-    output = subprocess.check_output(['textx', 'check', metamodel_file],
+    model_file = os.path.join(this_folder,
+                              'projects', 'flow_dsl', 'tests',
+                              'models', 'data_flow.eflow')
+    output = subprocess.check_output(['textx', 'check', model_file],
                                      stderr=subprocess.STDOUT)
     assert b'data_flow.eflow: OK.' in output
 
 
 def test_check_invalid_model():
-    metamodel_file = os.path.join(this_folder,
-                                  'projects', 'flow_dsl', 'tests',
-                                  'models', 'data_flow_including_error.eflow')
-    output = subprocess.check_output(['textx', 'check', metamodel_file],
+    model_file = os.path.join(this_folder,
+                              'projects', 'flow_dsl', 'tests',
+                              'models', 'data_flow_including_error.eflow')
+    output = subprocess.check_output(['textx', 'check', model_file],
                                      stderr=subprocess.STDOUT)
     assert b'error: types must be lowercase' in output
