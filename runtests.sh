cbc758773382b2e614623986845ab78407dac390
Igor Dejanovic <igor.dejanovic@gmail.com>
2019-04-04 11:12:32 +0200

    Omit six.py from test coverage

--- runtests.sh
+++ runtests.sh
@@ -2,5 +2,5 @@
 # Run all tests and generate coverage report
 
 coverage run --source textx -m py.test tests/functional || exit 1
-coverage report --fail-under 80 || exit 1
+coverage report --omit textx/six.py --fail-under 80 || exit 1
 flake8 || exit 1
