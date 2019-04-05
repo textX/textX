187e184cfccb2b50ff0bd93ff51f3e872122aa51
Igor Dejanovic <igor.dejanovic@gmail.com>
2019-04-05 10:27:52 +0200

    Improve tests for `check` command

--- tests/functional/registration/test_check.py
+++ tests/functional/registration/test_check.py
@@ -10,7 +10,7 @@ this_folder = os.path.abspath(os.path.dirname(__file__))
 
 def test_check_metamodel():
     """
-    Meta-model is also a model
+    Meta-model is also a model.
     """
 
     metamodel_file = os.path.join(this_folder,
@@ -32,6 +32,39 @@ def test_check_valid_model():
     assert 'data_flow.eflow: OK.' in result.output
 
 
+def test_check_valid_model_with_explicit_language_name():
+    """
+    Test checking of model where language name is given explicitly.
+    """
+    model_file = os.path.join(this_folder,
+                              'projects', 'flow_dsl', 'tests',
+                              'models', 'data_flow.eflow')
+    runner = CliRunner()
+    result = runner.invoke(textx, ['check',
+                                   '--language', 'flow-dsl',
+                                   model_file])
+    assert result.exit_code == 0
+    assert 'data_flow.eflow: OK.' in result.output
+
+
+def test_check_valid_model_with_metamodel_from_file():
+    """
+    Test checking of model where meta-model is provided from the grammar file.
+    """
+    model_file = os.path.join(this_folder,
+                              'projects', 'types_dsl', 'tests',
+                              'models', 'types.etype')
+    metamodel_file = os.path.join(this_folder,
+                                  'projects', 'types_dsl', 'types_dsl',
+                                  'Types.tx')
+    runner = CliRunner()
+    result = runner.invoke(textx, ['check',
+                                   '--grammar', metamodel_file,
+                                   model_file])
+    assert result.exit_code == 0
+    assert 'types.etype: OK.' in result.output
+
+
 def test_check_invalid_model():
     model_file = os.path.join(this_folder,
                               'projects', 'flow_dsl', 'tests',
@@ -40,3 +73,14 @@ def test_check_invalid_model():
     result = runner.invoke(textx, ['check', model_file])
     assert result.exit_code == 0
     assert 'error: types must be lowercase' in result.output
+
+
+def test_check_invalid_language():
+    """
+    Test calling check command with a file that is not registered.
+    """
+
+    runner = CliRunner()
+    result = runner.invoke(textx, ['check', 'some_unexisting_file'])
+
+    assert 'No language registered that can parse' in result.output
