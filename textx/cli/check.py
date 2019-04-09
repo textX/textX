85e3380d09a4cb0babc98c9e5a9eb44d93a11ceb
Igor Dejanovic <igor.dejanovic@gmail.com>
2019-04-09 18:52:24 +0200

    Optimize check command to instantiate meta-model only once if possible

--- textx/cli/check.py
+++ textx/cli/check.py
@@ -3,8 +3,8 @@ import os
 import click
 from textx import (metamodel_from_file,
                    metamodel_for_language,
-                   metamodel_for_file)
-from textx.exceptions import TextXError, TextXRegistrationError
+                   metamodel_for_file,
+                   TextXError, TextXRegistrationError)
 
 
 @click.argument('model_files', type=click.Path(), required=True, nargs=-1)
@@ -47,21 +47,25 @@ def check(ctx, model_files, language=None, grammar=None, ignore_case=False):
 
     debug = ctx.obj['debug']
 
-    for model_file in model_files:
-        try:
-            if grammar:
-                metamodel = metamodel_from_file(grammar, debug=debug,
-                                                ignore_case=ignore_case)
-            elif language:
-                metamodel = metamodel_for_language(language)
-            else:
+    try:
+        per_file_metamodel = False
+        if grammar:
+            metamodel = metamodel_from_file(grammar, debug=debug,
+                                            ignore_case=ignore_case)
+        elif language:
+            metamodel = metamodel_for_language(language)
+        else:
+            per_file_metamodel = True
+
+        for model_file in model_files:
+            if per_file_metamodel:
                 metamodel = metamodel_for_file(model_file)
 
             metamodel.model_from_file(model_file, debug=debug)
             click.echo("{}: OK.".format(os.path.abspath(model_file)))
 
-        except TextXRegistrationError as e:
-            click.echo(e.message)
+    except TextXRegistrationError as e:
+        click.echo(e.message)
 
-        except TextXError as e:
-            click.echo(e)
+    except TextXError as e:
+        click.echo(e)
