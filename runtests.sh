11891dd0ba54b42901f9ddb98a92f879a39f9d08
Igor Dejanovic <igor.dejanovic@gmail.com>
2019-04-21 20:30:45 +0200

    Add command for generating coverage report in html format

--- runtests.sh
+++ runtests.sh
@@ -3,4 +3,6 @@
 
 coverage run --source textx -m py.test tests/functional || exit 1
 coverage report --omit textx/six.py --fail-under 80 || exit 1
+# Run this to generate html report
+# coverage html --omit textx/six.py --directory=coverage
 flake8 || exit 1
