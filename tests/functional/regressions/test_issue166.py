79b1b21df152a5eb56973776c880886597ed5125
Igor Dejanovic <igor.dejanovic@gmail.com>
2019-04-27 19:40:45 +0200

    fix #166 Improve and simplify handling of abstract rule alternatives.

---
+++ tests/functional/regressions/test_issue166.py
@@ -0,0 +1,27 @@
+from __future__ import unicode_literals
+import textx
+
+
+def test_issue166_wrong_multiple_rule_reference():
+    """
+    Test wrongly detected referencing of multiple rules from a single abstract
+    rule alternative.
+    Reported in issue #166.
+    """
+    grammar = """
+    DSL:
+        commands*=BaseCommand;
+    BaseCommand:
+        (Require | Group) '.'?;
+
+    Require:
+        'require' /[a-z]/;
+    Group:
+        'group' hello=/[0-9]/;
+    """
+
+    metamodel = textx.metamodel_from_str(grammar)
+    assert metamodel
+    model = metamodel.model_from_str('require a. group 4')
+    assert model.commands[0] == 'requirea'
+    assert model.commands[1].hello == '4'
