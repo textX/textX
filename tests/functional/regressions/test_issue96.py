a7b3f0a86cc86bbff63caf92d4da5e27aaee3cf2
Igor Dejanovic <igor.dejanovic@gmail.com>
2019-04-27 17:40:07 +0200

    Add another test for issue #96

--- tests/functional/regressions/test_issue96.py
+++ tests/functional/regressions/test_issue96.py
@@ -29,3 +29,39 @@ def test_issue96_object_processor_on_match_rule_not_called():
 
     first_param = mymodel.parameters.parameters[0]
     assert isinstance(first_param, float)
+
+
+def test_issue96_object_processor_on_multiple_alternatives_not_called():
+
+    grammar = r"""
+    Command:
+        name=ID parameters=ParameterList
+    ;
+
+    ParameterList:
+        '(' parameters*=Parameter ')'
+    ;
+
+    Parameter:
+        value=ExplicitFloat | value=INT
+    ;
+
+    ExplicitFloat:
+        /-?\d+\.\d+/
+    ;
+    """
+
+    code = """
+    mycommand(0.1 1)
+    """
+    mm = metamodel_from_str(grammar)
+
+    mm.register_obj_processors({
+        'Parameter': lambda x: x.value,
+        'ExplicitFloat': lambda x: float(x)})
+
+    mymodel = mm.model_from_str(code)
+
+    first_param = mymodel.parameters.parameters[0]
+
+    assert isinstance(first_param, float)
