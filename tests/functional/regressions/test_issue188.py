99bf6d12993de84fd039e4f74a1291d6a3b160c8
goto40 <pierre.bayerl@googlemail.com>
2019-05-19 14:57:06 +0200

    skipws: more experiments

--- tests/functional/regressions/test_issue188.py
+++ tests/functional/regressions/test_issue188.py
@@ -11,3 +11,17 @@ def test_issue188_skipws_2():
         File: 'foo' ' ' 'bar';
     ''', skipws=False)
     program = mm.model_from_str('foo bar')
+
+def test_issue188_skipws_3():
+    mm = metamodel_from_str('''
+        File[noskipws]: 'foo' SPACE 'bar';
+        SPACE[noskipws]: /\s/;
+    ''')
+    program = mm.model_from_str('foo bar')
+
+def test_issue188_skipws_4():
+    mm = metamodel_from_str('''
+        File[noskipws]: 'foo' SPACE 'bar';
+        SPACE[noskipws]: ' ';
+    ''')
+    program = mm.model_from_str('foo bar')
