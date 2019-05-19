3d842e3afdee31f6fd8188485da14f23549871bd
Igor Dejanovic <igor.dejanovic@gmail.com>
2019-05-19 18:16:42 +0200

    Add one more test for #188

--- tests/functional/regressions/test_issue188.py
+++ tests/functional/regressions/test_issue188.py
@@ -1,27 +1,44 @@
-from textx.metamodel import metamodel_from_str
+from __future__ import unicode_literals
+import pytest
+from textx import metamodel_from_str, TextXSyntaxError
+
 
 def test_issue188_skipws_1():
-    mm = metamodel_from_str('''
+    mm = metamodel_from_str(r'''
         File: 'foo' /\s/ 'bar';
     ''', skipws=False)
-    program = mm.model_from_str('foo bar')
+    mm.model_from_str('foo bar')
+
 
 def test_issue188_skipws_2():
     mm = metamodel_from_str('''
         File: 'foo' ' ' 'bar';
     ''', skipws=False)
-    program = mm.model_from_str('foo bar')
+    mm.model_from_str('foo bar')
+
 
 def test_issue188_skipws_3():
-    mm = metamodel_from_str('''
+    mm = metamodel_from_str(r'''
         File[noskipws]: 'foo' SPACE 'bar';
         SPACE[noskipws]: /\s/;
     ''')
-    program = mm.model_from_str('foo bar')
+    mm.model_from_str('foo bar')
+
 
 def test_issue188_skipws_4():
     mm = metamodel_from_str('''
         File[noskipws]: 'foo' SPACE 'bar';
         SPACE[noskipws]: ' ';
     ''')
-    program = mm.model_from_str('foo bar')
+    mm.model_from_str('foo bar')
+
+
+def test_issue188_skipws_5():
+    mm = metamodel_from_str('''
+        File[noskipws]: ' foo' SPACE 'bar ';
+        SPACE[noskipws]: ' ';
+    ''')
+    with pytest.raises(TextXSyntaxError,
+                       match='.*Expected \'bar \' at position'):
+        mm.model_from_str(' foo bar')
+    mm.model_from_str(' foo bar ')
