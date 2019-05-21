6a994e60eff7bb12aff8b49a78a79e9f9ed69904
Igor Dejanovic <igor.dejanovic@gmail.com>
2019-05-21 14:26:13 +0200

    Fail tests if coverage is below 90%

--- runtests.sh
+++ runtests.sh
@@ -2,7 +2,7 @@
 # Run all tests and generate coverage report
 
 coverage run --omit textx/six.py --source textx -m py.test tests/functional || exit 1
-coverage report --fail-under 80 || exit 1
+coverage report --fail-under 90 || exit 1
 # Run this to generate html report
 # coverage html --omit textx/six.py --directory=coverage
 flake8 || exit 1
