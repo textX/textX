157f1fcfee61074e90274cddbcec14f1e2e522e3
Igor Dejanovic <igor.dejanovic@gmail.com>
2019-05-21 14:27:06 +0200

    Remove unneeded flag in calling coverage for html

--- runtests.sh
+++ runtests.sh
@@ -4,5 +4,5 @@
 coverage run --omit textx/six.py --source textx -m py.test tests/functional || exit 1
 coverage report --fail-under 90 || exit 1
 # Run this to generate html report
-# coverage html --omit textx/six.py --directory=coverage
+# coverage html --directory=coverage
 flake8 || exit 1
