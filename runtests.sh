3f7c1a1aa7431d6e3d7458f92d87fa0bd2d4e6ea
Igor Dejanovic <igor.dejanovic@gmail.com>
2019-05-21 14:20:19 +0200

    Fix omitting six.py in coverage report

--- runtests.sh
+++ runtests.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Run all tests and generate coverage report
 
-coverage run --source textx -m py.test tests/functional || exit 1
-coverage report --omit textx/six.py --fail-under 80 || exit 1
+coverage run --omit textx/six.py --source textx -m py.test tests/functional || exit 1
+coverage report --fail-under 80 || exit 1
 # Run this to generate html report
 # coverage html --omit textx/six.py --directory=coverage
 flake8 || exit 1
