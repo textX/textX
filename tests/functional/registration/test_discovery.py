c91aeed37f1e026499205a2f8a8d44bcb83b2876
Igor Dejanovic <igor.dejanovic@gmail.com>
2020-02-02 20:25:51 +0100

    refs #220 Add test for versions

--- tests/functional/registration/test_discovery.py
+++ tests/functional/registration/test_discovery.py
@@ -12,7 +12,7 @@ def test_list_languages_cli():
     runner = CliRunner()
     result = runner.invoke(textx, ['list-languages'])
     assert result.exit_code == 0
-    assert 'flow-dsl' in result.output
+    assert 'flow-dsl[1.0.0]' in result.output
     assert '*.eflow' in result.output
     assert 'data-dsl' in result.output
 
@@ -25,3 +25,4 @@ def test_list_generators_cli():
     result = runner.invoke(textx, ['list-generators'])
     assert result.exit_code == 0
     assert 'flow-dsl -> PlantUML' in result.output
+    assert 'flow-codegen[1.0.0]' in result.output
