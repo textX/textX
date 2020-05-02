c305e1bade33fb62ce06a0ee79fb4dbf3160d37f
Igor Dejanovic <igor.dejanovic@gmail.com>
2020-05-02 16:14:53 +0200

    ref #246 Fix applying multiple rule modifiers

---
+++ tests/functional/regressions/test_issue246.py
@@ -0,0 +1,26 @@
+from __future__ import unicode_literals
+from textx import metamodel_from_str
+
+
+def test_multiple_rule_modifiers():
+    """
+    Test that multiple rule modifiers are applied correctly.
+    See: https://github.com/textX/textX/issues/246
+    """
+
+    grammar = r'''
+    main_rule[ws=' \t']:
+        "word1" rule1
+    ;
+
+    rule1[skipws, ws=' \t\n']:
+        "word2" "word3"
+    ;
+    '''
+
+    meta = metamodel_from_str(grammar)
+    s = '''word1 word2
+    word3'''
+
+    model = meta.model_from_str(s)
+    assert model == 'word1word2word3'
