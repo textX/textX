8211c32e48ba8db546403ed27b15f61f3d52807a
goto40 <pierre.bayerl@googlemail.com>
2020-08-08 12:20:06 +0200

    initial tests / implementation of find

--- tests/functional/test_scoping/test_rrel.py
+++ tests/functional/test_scoping/test_rrel.py
@@ -1,6 +1,7 @@
 from textx.scoping.rrel import rrel, parse
 from arpeggio import ParserPython
-
+from textx import metamodel_from_str
+from textx.scoping.rrel import find
 
 def test_rrel_basic_parser1():
     parser = ParserPython(rrel)
@@ -16,9 +17,9 @@ def test_rrel_basic_parser1():
 
 def test_rrel_basic_parser2():
     tree = parse("^pkg*.cls")
-    assert str(tree) == '(..)*.pkg*.cls'
+    assert str(tree) == '(..)*.(pkg)*.cls'
     tree = parse("obj.ref.~extension *.methods")
-    assert str(tree) == 'obj.ref.~extension*.methods'
+    assert str(tree) == 'obj.ref.(~extension)*.methods'
     tree = parse("type.vals")
     assert str(tree) == 'type.vals'
     tree = parse("(type.vals)")
@@ -27,3 +28,80 @@ def test_rrel_basic_parser2():
     assert str(tree) == '(type.vals)*'
     tree = parse("instance . ( type.vals ) *")
     assert str(tree) == 'instance.(type.vals)*'
+
+
+def test_rrel_basic_lookup():
+    """
+    This is a basic test for the find function
+    """
+    #################################
+    # META MODEL DEF
+    #################################
+    metamodel_str = '''
+        Model:
+            packages*=Package
+        ;
+
+        Package:
+            'package' name=ID '{'
+            classes*=Class
+            '}'
+        ;
+
+        Class:
+            'class' name=ID '{'
+                attributes*=Attribute
+            '}'
+        ;
+
+        Attribute:
+                'attr' name=ID ';'
+        ;
+
+        Comment: /#.*/;
+        FQN: ID('.'ID)*;
+        '''
+
+    my_metamodel = metamodel_from_str(metamodel_str)
+
+    #################################
+    # MODEL PARSING
+    #################################
+
+    my_model = my_metamodel.model_from_str('''
+    package P1 {
+        class Part1 {
+        }
+    }
+    package P2 {
+        class Part2 {
+            attr rec;
+        }
+        class C2 {
+            attr p1;
+            attr p2a;
+            attr p2b;
+        }
+    }
+    ''')
+
+    #################################
+    # TEST MODEL
+    #################################
+
+    P2 = find(my_model, "P2", "packages")
+    assert P2 is not None
+    Part2 = find(my_model, "P2.Part2", "packages.classes")
+    assert Part2 is not None
+    rec = find(my_model, "P2.Part2.rec", "packages.classes.attributes")
+    assert rec is not None
+
+    P2 = find(my_model, "P2", "(packages)")
+    assert P2 is not None
+
+    P2 = find(my_model, "P2", "packages*")
+    assert P2 is not None
+    Part2 = find(my_model, "P2.Part2", "packages*.classes")
+    assert Part2 is not None
+    rec = find(my_model, "P2.Part2.rec", "packages*.classes.attributes")
+    assert rec is not None
--- textx/scoping/rrel.py
+++ textx/scoping/rrel.py
@@ -1,3 +1,6 @@
+from textx import get_metamodel, textx_isinstance
+from textx.scoping.tools import needs_to_be_resolved
+from textx.scoping import Postponed
 from arpeggio import Optional, EOF
 from arpeggio import ZeroOrMore as ArpeggioZeroOrMore
 from arpeggio import RegExMatch as _
@@ -49,6 +52,22 @@ class Parent:
     def __repr__(self):
         return 'parent({})'.format(self.type)
 
+    def apply(self, obj, lookup_list):
+        """
+        Args:
+            obj: model object
+            lookup_list: non-empty name list (not used)
+
+        Returns:
+            The parent of the specified type or None.
+        """
+        t = get_metamodel(obj)[self.type]
+        while hasattr(obj, "parent"):
+            obj = obj.parent
+            if textx_isinstance(obj, t):
+                return obj, lookup_list
+        return None, lookup_list
+
 
 class Navigation:
     def __init__(self, name, consume_name):
@@ -56,7 +75,44 @@ class Navigation:
         self.consume_name = consume_name
 
     def __repr__(self):
-        return '~' + self.name if self.consume_name else self.name
+        return self.name if self.consume_name else '~' + self.name
+
+    def apply(self, obj, lookup_list):
+        """
+        Args:
+            obj: model object
+            lookup_list: non-empty name list
+
+        Returns:
+            The object indicated by the navigation object,
+            Postponed, None, or a list (if a list has to be processed).
+        """
+        if needs_to_be_resolved(obj, self.name):
+            return Postponed()
+        if hasattr(obj, self.name):
+            target = getattr(obj, self.name)
+            if isinstance(target, list):
+                if not self.consume_name:
+                    return target, lookup_list  # return list
+                else:
+                    lst = list(filter(lambda x: hasattr(
+                        x, "name") and getattr(
+                        x, "name") == lookup_list[0], target))
+                    if len(lst) > 0:
+                        return lst[0], lookup_list[1:]  # return obj
+                    else:
+                        return None, lookup_list  # return None
+            else:
+                if not self.consume_name:
+                    return target, lookup_list
+                else:
+                    if hasattr(target, "name") and getattr(
+                            target, "name") == lookup_list[0]:
+                        return target, lookup_list[1:]  # return obj
+                    else:
+                        return None, lookup_list  # return None
+        else:
+            return None, lookup_list
 
 
 class Brackets:
@@ -74,10 +130,31 @@ class Dots:
     def __repr__(self):
         return ('.' * self.num)
 
+    def apply(self, obj, lookup_list):
+        """
+        Args:
+            obj: model object
+            lookup_list: non-empty name list (not used)
+
+        Returns:
+            The parent or None.
+        """
+        num = self.num
+        while num > 1 and hasattr(obj, "parent"):
+            obj = obj.parent
+            num -= 1
+        if num <= 1:
+            return obj, lookup_list
+        else:
+            return None, lookup_list
+
 
 class ZeroOrMore:
     def __init__(self, path_element):
+        if not isinstance(path_element, Brackets):
+            path_element = Brackets(Path([path_element]))
         self.path_element = path_element
+        assert(isinstance(self.path_element, Brackets))
 
     def __repr__(self):
         return str(self.path_element) + '*'
@@ -85,7 +162,7 @@ class ZeroOrMore:
 
 class Path:
     def __init__(self, path_elements):
-        print("create Path :" + str(path_elements))
+        # print("create Path :" + str(path_elements))
         self.path_elements = path_elements
         if (self.path_elements[0] == '^'):
             self.path_elements[0] = ZeroOrMore(Brackets(Path([Dots(2)])))
@@ -105,9 +182,9 @@ class RrelVisitor(PTNodeVisitor):
 
     def visit_navigation(self, node, children):
         if len(children) == 1:
-            return Navigation(children[0], False)
+            return Navigation(children[0], True)
         else:
-            return Navigation(children[1], True)
+            return Navigation(children[1], False)
 
     def visit_brackets(self, node, children):
         assert(len(children) == 1)  # a path
@@ -143,18 +220,63 @@ def parse(rrel_expression):
     return visit_parse_tree(parse_tree, RrelVisitor())
 
 
-def find(obj, lookup_string, rrel_tree):
+def find(obj, lookup_list, rrel_tree):
     """
     This function gets all/one element from a model
     object based on an rrel tree (query).
 
     Args:
         obj: model object (starting point of the search)
-        lookup_string: dot sepaarted name
-        rrel_tree: the query
+        lookup_list: list of name parts forming the full name.
+        rrel_tree: the query (must be a Path oebjct)
 
     Returns:
         The result of the query (first match), a
-        Postponed object, or None (nothing found).
+        Postponed object, or None (nothing found)
     """
-    pass
+    if isinstance(rrel_tree, str):
+        rrel_tree = parse(rrel_tree)
+    if isinstance(lookup_list, str):
+        lookup_list = lookup_list.split(".")
+
+    def apply(obj, lookup_list, p, idx=0):
+        assert isinstance(p, Path)
+        assert len(p.path_elements) >= idx
+        # assert len(lookup_list) > 0
+        for e in p.path_elements[idx:]:
+            if hasattr(e, "apply"):
+                obj, lookup_list = e.apply(obj, lookup_list)
+                if isinstance(obj, Postponed):
+                    return obj, lookup_list
+            elif isinstance(e, Brackets):
+                obj, lookup_list = apply(obj, lookup_list, e.path)
+                if isinstance(obj, Postponed):
+                    return obj, lookup_list
+            elif isinstance(e, ZeroOrMore):
+                obj_temp = obj
+                lookup_list_temp = lookup_list
+                visited = set()
+                while obj_temp is not None and len(lookup_list_temp) > 0:
+                    obj_res, lookup_list_res = apply(obj_temp, lookup_list_temp, p, idx+1)
+                    if obj_res is not None and (
+                            isinstance(obj_res, Postponed) or len(lookup_list_res)==0):
+                        return obj_res, lookup_list_res  # found match / postponed
+                    obj_temp, lookup_list_temp = apply(
+                        obj_temp, lookup_list_temp, e.path_element.path)
+                    if obj_temp in visited:
+                        break
+                    if obj_temp is not None and (
+                            isinstance(obj_temp, Postponed) or len(lookup_list_res)==0):
+                        return obj_temp, lookup_list_temp  # found match / postponed
+                    visited.add(obj_temp)
+                return obj_temp, lookup_list_temp
+            idx += 1
+        return obj, lookup_list
+
+    obj_res, lookup_list_res = apply(obj, lookup_list, rrel_tree)
+    if isinstance(obj_res, Postponed):
+        return obj_res  # Postponed
+    elif obj_res is not None and len(lookup_list_res) == 0:
+        return obj_res  # found match
+    else:
+        return None  # not found
