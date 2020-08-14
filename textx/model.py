5cbd2cceb9d455b1b243461f19fe55b88f6b49e6
goto40 <pierre.bayerl@googlemail.com>
2020-08-14 11:47:05 +0200

    added an assertion to make sure we did not forget a case.

--- textx/model.py
+++ textx/model.py
@@ -304,6 +304,8 @@ def get_model_parser(top_rule, comments_model, **kwargs):
                 if self.metamodel.allow_unused_user_classes:
                     # ignore class (no replacement of methods)
                     return
+                else:
+                    assert False, "unexpected, user_class has no _tx_obj_attrs"
 
             # Custom attr dunder methods used for user classes during loading
             def _getattribute(obj, name):
