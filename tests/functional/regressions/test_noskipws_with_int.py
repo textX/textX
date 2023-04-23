de305b192d3695d52ab4d5f18ba5743f059ae045
Nevena Alavanja <nevena.alavanja@levi9.com>
2023-04-23 16:39:20 +0200

    Added test for noskipws with int rule

---
+++ tests/functional/regressions/test_noskipws_with_int.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+from textx import metamodel_from_str
+
+
+def test_noskipws_modifier_with_int_rule():
+    """
+    Tests that noskipsws rule modifier works with INT rule.
+    """
+    grammar = r'''
+    AssessmentEnd:
+        'completion_time' ':' /\s*/ completion_time=TimeHours
+    ;
+
+    TimeHours[noskipws]:
+        hours=INT 'h'
+    ;
+    '''
+
+    meta = metamodel_from_str(grammar)
+    s = '''completion_time: 1h'''
+
+    model = meta.model_from_str(s)
+    assert model is not None
