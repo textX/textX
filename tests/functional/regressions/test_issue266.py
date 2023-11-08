9feddfd73cf0d1fd5cfacc334486a24902500948
Igor Dejanovic <igor.dejanovic@gmail.com>
2023-11-08 17:13:42 +0100

    add old regression test

---
+++ tests/functional/regressions/test_issue266.py
@@ -0,0 +1,17 @@
+from __future__ import unicode_literals
+import pytest
+from textx import metamodel_from_str
+from textx.exceptions import TextXSemanticError
+
+
+def test_unhashable_type_for_name():
+    '''
+    Test that using unhashable type for name of model object is correctly reported.
+    '''
+
+    mm = metamodel_from_str('''
+    TestObj: name+=ID['.'];
+    ''')
+
+    with pytest.raises(TextXSemanticError, match='.*of unhashable type.*'):
+        mm.model_from_str('test')
