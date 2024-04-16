cf4e62f3880a63a79ecc68057882445539b41017
Igor Dejanovic <igor.dejanovic@gmail.com>
2024-04-16 23:59:52 +0200

    docs: mdbook docker image

---
+++ docker/publish.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+#  Login with:
+#       docker login -u igordejanovic
+docker push igordejanovic/mdbook-textx:latest
---
+++ docker/test.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker run --rm igordejanovic/mdbook-textx:latest mdbook
