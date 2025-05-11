1cf31a8d9e256cc2674057805f01f52d80f9fcde
Igor Dejanovic <igor.dejanovic@gmail.com>
2025-05-11 11:03:13 +0200

    test: fix tests after click upgrade to 8.2.0

--- tests/functional/subcommands/test_subcommands.py
+++ tests/functional/subcommands/test_subcommands.py
@@ -5,14 +5,15 @@ def test_subcommand():
     """
     Test that a command from the example project is registered.
     """
-    output = subprocess.check_output(["textx"], stderr=subprocess.STDOUT)
-    assert b"testcommand" in output
+    result = subprocess.run(["textx"], capture_output=True, check=False)
+    assert b"testcommand" in (result.stdout + result.stderr)
 
 
 def test_subcommand_group():
     """
     Test that a command group is registered.
     """
-    output = subprocess.check_output(["textx", "testgroup"], stderr=subprocess.STDOUT)
-    assert b"groupcommand1" in output
-    assert b"groupcommand2" in output
+    result = subprocess.run(["textx", "testgroup"],
+                            capture_output=True, check=False)
+    assert b"groupcommand1" in (result.stdout + result.stderr)
+    assert b"groupcommand2" in (result.stdout + result.stderr)
